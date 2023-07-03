0a3e97002919f1fab938f82dc43367a11b96fecc
Moesha <moesha.touzeyidio@gmail.com>
2023-07-03 10:22:01 +0200

    [add] Add structure base of springBoot project.

---
+++ script.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE werewolf;
+CREATE USER 'projet'@'localhost' IDENTIFIED BY 'Projet_LoupGarou1';
+GRANT ALL ON werewolf.* TO 'projet'@'localhost';
---
+++ src/test/java/fr/ensitech/projet/DevApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ensitech.projet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
