99c8b67cb941dfe255a0ba77b627276e87e94b15
Moesha <moesha.touzeyidio@gmail.com>
2023-07-04 22:39:20 +0200

    [Modify] Add method for recover wolf and villager role.

--- src/main/java/fr/ensitech/projet/repository/RoleRepository.java
+++ src/main/java/fr/ensitech/projet/repository/RoleRepository.java
@@ -1,9 +1,19 @@
 package fr.ensitech.projet.repository;
 
 import fr.ensitech.projet.entity.Role;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RoleRepository extends CrudRepository<Role, Integer> {
+
+    @Query(value = "SELECT * FROM role WHERE wolf = 1 " ,nativeQuery = true)
+    List<Role> getAllWolfRole();
+
+    @Query(value = "SELECT * FROM role WHERE villager = 1", nativeQuery = true)
+    List<Role> getAllVillagerRole();
+
 }
