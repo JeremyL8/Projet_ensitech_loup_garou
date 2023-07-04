c5a6639dfd69f06e88059ebb81d9080402336bdf
Moesha <moesha.touzeyidio@gmail.com>
2023-07-04 09:59:18 +0200

    [modify] Modify name of tables for fixe problem.

--- script.sql
+++ script.sql
@@ -2,21 +2,25 @@ CREATE DATABASE werewolf;
 CREATE USER 'projet'@'localhost' IDENTIFIED BY 'Projet_LoupGarou1';
 GRANT ALL ON werewolf.* TO 'projet'@'localhost';
 
-CREATE TABLE Users ( 
-    id INT PRIMARY KEY NOT NULL,
+USE werewolf;
+
+CREATE TABLE users (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     userName VARCHAR(100),
     pwd VARCHAR(100),
     levels VARCHAR(100)
 );
-CREATE TABLE Player ( 
-    id INT PRIMARY KEY NOT NULL,
-    idUser INT REFERENCES Users(id),
-    statut VARCHAR(100),
-    idRole INT REFERENCES Role(id) 
-);
-CREATE TABLE Role ( 
-    id INT PRIMARY KEY NOT NULL,
+CREATE TABLE role (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     roleName VARCHAR(100),
     descript VARCHAR(100),
     picture VARCHAR(1000)
+);
+CREATE TABLE player (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    idUser INT ,
+    statut VARCHAR(100),
+    idRole INT,
+    FOREIGN KEY (idUser) REFERENCES Users(id),
+    FOREIGN KEY (idRole) REFERENCES Role(id)
 );
